a5c0ad25a25de6c443711cacf9641dad979cb824
Feyto <feyto81@gmail.com>
2021-03-24 22:55:30 +0700

    editing public asset file

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -1534,7 +1534,7 @@
         <script src="{{asset('admin/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
         <script src="{{asset('admin/assets/libs/metismenu/metisMenu.min.js')}}"></script>
         <script src="{{asset('admin/assets/libs/simplebar/simplebar.min.js')}}"></script>
-        <script src="{{asset('admin/assets/libs/node-waves/waves.min.js')}}"></script>
+        <script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
 
         <!-- apexcharts -->
         <script src="{{asset('admin/assets/libs/apexcharts/apexcharts.min.js')}}"></script>
