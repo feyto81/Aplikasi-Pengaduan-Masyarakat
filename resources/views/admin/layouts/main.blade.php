4238af84b0f13f23b467268bd045702209a847b5
Feyto <feyto81@gmail.com>
2021-03-26 21:10:45 +0700

    partailing

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -1,96 +1,10 @@
 
 <!doctype html>
 <html lang="en">
-<head>
-        
-        <meta charset="utf-8" />
-        <title>Colored Sidebar | Skote - Admin & Dashboard Template</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-        <meta content="Themesbrand" name="author" />
-        <link rel="shortcut icon" href="{{asset('admin/assets/images/favicon.ico')}}">
-        <link href="{{asset('admin/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-        <link href="{{asset('admin/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{asset('admin/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
-    </head>
+    @include('admin.partials.head')
     <body data-sidebar="colored">
         <div id="layout-wrapper">
-            <header id="page-topbar">
-                <div class="navbar-header">
-                    <div class="d-flex">
-                        <div class="navbar-brand-box">
-                            <a href="index.html" class="logo logo-dark">
-                                <span class="logo-sm">
-                                    <img src="{{asset('admin/assets/images/logo.svg')}}" alt="" height="22">
-                                </span>
-                                <span class="logo-lg">
-                                    <img src="{{asset('admin/assets/images/logo-dark.png')}}" alt="" height="17">
-                                </span>
-                            </a>
-
-                            <a href="index.html" class="logo logo-light">
-                                <span class="logo-sm">
-                                    <img src="{{asset('admin/assets/images/logo-light.svg')}}" alt="" height="22">
-                                </span>
-                                <span class="logo-lg">
-                                    <img src="{{asset('admin/assets/images/logo-light.png')}}" alt="" height="19">
-                                </span>
-                            </a>
-                        </div>
-
-                        <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect" id="vertical-menu-btn">
-                            <i class="fa fa-fw fa-bars"></i>
-                        </button>
-
-                    </div>
-
-                    <div class="d-flex">
-
-                        <div class="dropdown d-inline-block d-lg-none ms-2">
-                            <button type="button" class="btn header-item noti-icon waves-effect" id="page-header-search-dropdown"
-                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="mdi mdi-magnify"></i>
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0"
-                                aria-labelledby="page-header-search-dropdown">
-        
-                                <form class="p-3">
-                                    <div class="form-group m-0">
-                                        <div class="input-group">
-                                            <input type="text" class="form-control" placeholder="Search ..." aria-label="Recipient's username">
-                                            <div class="input-group-append">
-                                                <button class="btn btn-primary" type="submit"><i class="mdi mdi-magnify"></i></button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="dropdown d-none d-lg-inline-block ms-1">
-                            <button type="button" class="btn header-item noti-icon waves-effect" data-toggle="fullscreen">
-                                <i class="bx bx-fullscreen"></i>
-                            </button>
-                        </div>
-
-
-                        <div class="dropdown d-inline-block">
-                            <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
-                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <img class="rounded-circle header-profile-user" src="{{asset('admin/assets/images/users/avatar-1.jpg')}}"
-                                    alt="Header Avatar">
-                                <span class="d-none d-xl-inline-block ms-1" key="t-henry">Henry</span>
-                                <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
-                            </button>
-                            <div class="dropdown-menu dropdown-menu-end">
-                                <!-- item-->
-                                <a class="dropdown-item" href="#"><i class="bx bx-user font-size-16 align-middle me-1"></i> <span key="t-profile">Profile</span></a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item text-danger" href="#"><i class="bx bx-power-off font-size-16 align-middle me-1 text-danger"></i> <span key="t-logout">Logout</span></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </header>
+            @include('admin.partials.header')
             <div class="vertical-menu">
                 <div data-simplebar class="h-100">
                     <div id="sidebar-menu">
@@ -154,7 +68,6 @@
                                     <h4 class="mb-sm-0 font-size-18">Colored Sidebar</h4>
                                     <div class="page-title-right">
                                         <ol class="breadcrumb m-0">
-                                            <li class="breadcrumb-item"><a href="javascript: void(0);">Layouts</a></li>
                                             <li class="breadcrumb-item active">Colored Sidebar</li>
                                         </ol>
                                     </div>
@@ -286,17 +199,8 @@
                     </div>
                 </footer>
             </div>
-            <!-- end main content-->
-
         </div>
         <div class="rightbar-overlay"></div>
-        <script src="{{asset('admin/assets/libs/jquery/jquery.min.js')}}"></script>
-        <script src="{{asset('admin/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-        <script src="{{asset('admin/assets/libs/metismenu/metisMenu.min.js')}}"></script>
-        <script src="{{asset('admin/assets/libs/simplebar/simplebar.min.js')}}"></script>
-        <script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
-        <script src="{{asset('admin/assets/libs/apexcharts/apexcharts.min.js')}}"></script>
-        <script src="{{asset('admin/assets/js/pages/dashboard.init.js')}}"></script>
-        <script src="{{asset('admin/assets/js/app.js')}}"></script>
+        @include('admin.partials.script')
     </body>
 </html>
---
+++ resources/views/admin/partials/head.blade.php
@@ -0,0 +1,12 @@
+<head>
+        
+    <meta charset="utf-8" />
+    <title>@yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="" name="description" />
+    <meta content="" name="author" />
+    <link rel="shortcut icon" href="{{asset('admin/assets/images/favicon.ico')}}">
+    <link href="{{asset('admin/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <link href="{{asset('admin/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('admin/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
+</head>
